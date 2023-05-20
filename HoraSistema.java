148984f809bd436c039a7fa069523c2bf5498639
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-19 23:00:19 -0300

    Feito dois projetos que recebem data hora e linguagem do sistema

---
+++ HoraSistema.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+    class DataHora{
+        public static void main(String[] args){
+            Date hora = new Date();
+            System.out.println("Horário:");
+            System.out.println(hora.toString());
+        }
+    }
+
+
+
+
