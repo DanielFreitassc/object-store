f153a6deee0f00655a618dcd48b7a7167640230b
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-27 22:14:32 -0300

    app finaces

--- javaestudos/src/javaestudos/Appfinances.java
+++ javaestudos/src/javaestudos/Appfinances.java
@@ -1,5 +1,21 @@
 package javaestudos;
-
+import java.util.Scanner; 
 public class Appfinances {
-
+	public static void main(String[] args) {
+		Scanner dados = new Scanner(System.in);
+		System.out.print("Insira suas dispesas mensais: ");
+		float dispesa = dados.nextFloat();
+		
+		System.out.print("Insira o seu salário mensal: ");
+		float salario = dados.nextFloat();
+		
+		System.out.print("Se receber dinheiro extra adicione aqui senão digite: (0) :  ");
+		float extra = dados.nextFloat();
+		float total = salario + extra;
+		System.out.println(" Seu dinheiro nesse mês é: "+ total);
+		System.out.println(" Suas dispesas são: "+ dispesa);
+		System.out.println(" Sobrara no final do mês: "+ (total-dispesa));
+		dados.close();
+	}
+	
 }
