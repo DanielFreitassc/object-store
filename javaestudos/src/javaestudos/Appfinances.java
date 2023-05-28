48aa83080e7861fc61405d0245a67a4b70acfcf0
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-27 23:26:28 -0300

    App de finanças atualização

--- javaestudos/src/javaestudos/Appfinances.java
+++ javaestudos/src/javaestudos/Appfinances.java
@@ -1,8 +1,14 @@
 package javaestudos;
 import java.util.Scanner; 
 public class Appfinances {
+	private static String nome;
 	public static void main(String[] args) {
+	 	
 		Scanner dados = new Scanner(System.in);
+		
+		System.out.print("Insira seu nome: ");
+		String nome = dados.nextLine();
+		
 		System.out.print("Insira suas dispesas mensais: ");
 		float dispesa = dados.nextFloat();
 		
@@ -12,9 +18,15 @@ public class Appfinances {
 		System.out.print("Se receber dinheiro extra adicione aqui senão digite: (0) :  ");
 		float extra = dados.nextFloat();
 		float total = salario + extra;
+		System.out.println("Olá "+ nome + " Seja Bem Vindo(a)! ");
 		System.out.println(" Seu dinheiro nesse mês é: "+ total);
-		System.out.println(" Suas dispesas são: "+ dispesa);
+		System.out.println(" Suas despesas são: "+ dispesa);
+		if (total-dispesa >=0) {
 		System.out.println(" Sobrara no final do mês: "+ (total-dispesa));
+		}
+		else {
+		System.out.println("Você está devendo: "+ (total-dispesa));
+		}
 		dados.close();
 	}
 	
