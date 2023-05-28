a034772ef6ebc92ca72985f50d153f8d66906f3d
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-27 21:44:07 -0300

    laço de repetição

---
+++ javaestudos/src/javaestudos/Appfinances.java
@@ -0,0 +1,5 @@
+package javaestudos;
+
+public class Appfinances {
+
+}
---
+++ javaestudos/src/javaestudos/repeticao.java
@@ -0,0 +1,18 @@
+package javaestudos;
+
+public class repeticao {
+	public static void main(String[] args) {
+	int i = 0;
+	while (i < 10) {
+	System.out.print("Imprimido ");
+	i++; 
+	
+	
+	
+	
+	
+	
+	
+		}	
+	}
+}
