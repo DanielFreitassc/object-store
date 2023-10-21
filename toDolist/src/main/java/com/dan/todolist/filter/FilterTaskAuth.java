3d63e0f6710d13c21ef408285799c8d7db779c3c
Daniel Freitas <danielfreitassc@gmail.com>
2023-10-21 14:09:41 -0300

    att

--- toDolist/src/main/java/com/dan/todolist/filter/FilterTaskAuth.java
+++ toDolist/src/main/java/com/dan/todolist/filter/FilterTaskAuth.java
@@ -3,46 +3,66 @@ package com.dan.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.dan.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired 
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                var authorization = request.getHeader("Authorization");
+        var servletPath = request.getServletPath();
 
-                var authEncoded = authorization.substring("basic".length()).trim();
+        if(servletPath.equals("/tasks/")){
+                // Pegar a autenticação do (usuario e senha)
+            var authorization = request.getHeader("Authorization");
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authEncoded = authorization.substring("basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
                 
-                var authString = new String(authDecode);
+            var authString = new String(authDecode);
 
-               
-                
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+                      
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
 
-                System.out.println("Authorization");
-                System.out.println(username);
-                System.out.println(password);
+            //Validar usuário
+            var user = this.userRepository.findByUsername(username);
+            if(user == null){
+                response.sendError(401);
+            }else{
+                // Validar senha   
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerify.verified){
+                    request.setAttribute("idUser",user.getId());
+                    filterChain.doFilter(request, response);
+                } else{
+                    response.sendError(401);
+                }
 
-                
-                filterChain.doFilter(request, response);
+            }
+                       
+        }else{
+          filterChain.doFilter(request, response);
+        }
+    
     }
 
- 
-    
 }
--- toDolist/src/main/java/com/dan/todolist/task/TaskCrontroller.java
+++ toDolist/src/main/java/com/dan/todolist/task/TaskCrontroller.java
@@ -1,10 +1,24 @@
 package com.dan.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.aspectj.weaver.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,9 +28,39 @@ public class TaskCrontroller {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
-        System.out.println("Chegou no controller");
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        
+        var currentDate = LocalDateTime.now();
+        //Validadador de data
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de inicíco / data de término deve ser maior que data atual");
+        }
+
+         if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início dever ser menor que aa data de término");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+    
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+    
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+        var task = this.taskRepository.findRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+        
+        return this.taskRepository.save(task);
     }
 }
