f96a781cbc50205db6bc17990b227eea5a8ce062
danielfreitassc <danielfreitassc@gmail.com>
2024-06-21 10:43:19 -0300

    Remember java types

---
+++ FirstClass.java
@@ -0,0 +1,19 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        int typeInt = 10;
+        double typeDouble = 3.14;
+        float typeFloat = 3.14F;
+        long typeLong = 999999999999999999L;
+        String typeString = "Text";
+        char caracter = 'M';
+        boolean typeBoolean = true;
+        byte typeNumber = 99;
+        
+        Integer typeWrapperInteger = 10;
+        Double typeWrapperDouble = 3.14;
+        Float typeWrapperFloat = 3.14F;
+        Long typeWrapperLong = 999999999999999999L;
+        Boolean typeWrapperBoolean = false;
+        Byte typeWrapperByte = 99;
+    }
+}
