da32e056cf2948086e68bc279c75e53eb7ec6703
danielfreitassc <danielfreitassc@gmail.com>
2024-06-22 14:41:34 -0300

    Feat: Aula 16 um pouco de wrapper classes

---
+++ wapper/Wrapper.java
@@ -0,0 +1,13 @@
+package wapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1F;
+        boolean tipoBoolean = false;
+
+        Integer wrapperInt = 1;
+        Float wrapperFloat = 1F;
+        Boolean wrapperBoolean = false;
+    }
+}
