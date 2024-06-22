c6792123cac41fd63ac727faf453580f9540d897
danielfreitassc <danielfreitassc@gmail.com>
2024-06-22 14:31:31 -0300

    Feat: Aula 15 um pouco sobre construtores

---
+++ construtor/ConstrutorController.java
@@ -0,0 +1,9 @@
+package construtor;
+
+public class ConstrutorController {
+    public static void main(String[] args) {
+        
+        Construtor construtor = new Construtor(1);
+        System.out.println(construtor.getNumero());
+    }
+}
