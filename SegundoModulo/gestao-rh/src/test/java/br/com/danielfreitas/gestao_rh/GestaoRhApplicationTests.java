36847fdfe62fddb199cd17d19fff3afdda956df4
danielfreitassc <danielfreitassc@gmail.com>
2024-06-23 21:32:53 -0300

    Feat: Aula 04 conceitos do springboot

---
+++ SegundoModulo/gestao-rh/src/test/java/br/com/danielfreitas/gestao_rh/GestaoRhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielfreitas.gestao_rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
