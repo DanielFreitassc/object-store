3c614dddc34e5c0f739df9eec21b273ed91eae8a
danielfreitassc <danielfreitassc@gmail.com>
2024-07-06 23:59:59 -0300

    Feat: modulo 2

--- SegundoModulo/segundo_modulo/src/main/java/com/daniel/segundo_modulo/Controllers/SecondModuleContoller.java
+++ SegundoModulo/segundo_modulo/src/main/java/com/daniel/segundo_modulo/Controllers/SecondModuleContoller.java
@@ -3,15 +3,20 @@ package com.daniel.segundo_modulo.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class SecondModuleContoller {
 
-    @GetMapping("/{num1}/{num2}")
+    @GetMapping("/soma/{num1}/{num2}")
     public double PathParms(@PathVariable double num1, @PathVariable double num2) {
         return num1 + num2;
     }
     
+    @GetMapping("/soma")
+    public double queryParams(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + num2;
+    }
 }
